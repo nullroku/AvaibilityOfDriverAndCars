9e96f4c544579d1884e64fddaa23be300851408c
Illiyan Ainun Qalbi <nullroku@gmail.com>
2017-05-12 10:24:48 +0800

    Car

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,8 +52,10 @@
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}">Login</a></li>
                         @else
+                            <li><a href="/car">Mobil</a></li>
                             <li><a href="/user">User</a></li>
-                            <li class="dropdown">                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
